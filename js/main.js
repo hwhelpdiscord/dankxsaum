3fc28c838a710c8894ccfcfb9c05cd37dd704331
hwhelpdiscord <67571912+hwhelpdiscord@users.noreply.github.com>
2020-07-27 12:30:02 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -303,7 +303,7 @@
 
 	function makeTimer() {
 
-		var endTime = new Date("28 July 2020 9:56:00 GMT+01:00");			
+		var endTime = new Date("28 July 2020 9:56:00 CST+06:00");			
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
